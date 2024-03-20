579f90a5052759a520390df0b56aef338c5e3df8
aaron nguyen <43309718+nguyenthanh1205tb@users.noreply.github.com>
2024-03-20 09:57:38 +0700

    revert get media limit

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -25,7 +25,7 @@ const useListMedia = () => {
   const [totalCount, setTotalCount] = useState(0)
 
   const getListMedia = (payload?: Partial<GetListMediaRequest>) => {
-    const queries = { ...listMediaQueries, ...payload, take: 10 }
+    const queries = { ...listMediaQueries, ...payload }
     return useQuery<GetListMediaResponse>({
       queryKey: ['getListMedia', queries],
       queryFn: () =>
