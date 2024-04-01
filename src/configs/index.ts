5d712c8374b4f34f83023c26f73f15e1a65eaa4e
Yang <dethia1toanlyanh@gmail.com>
2024-04-01 16:21:50 +0700

    [add] upload from url

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -4,6 +4,8 @@ const ORG_ID = '65ae3593905f66bff177f068'
 const TEMPLATE_ID = '65bc649a1f2422e6c787898d'
 const APP_API_URL = 'https://api.plcplatform.net'
 const REACT_APP_DOWNLOAD_ENDPOINT = 'https://download.plcplatform.net'
+const FB_YT_IN_STRING_REGEX =
+  /^(?:https?:\/\/)?(?:www\.)?(youtube\.com|facebook\.com|fb\.com|m\.facebook\.com|facebook\.watch|fb\.watch|m\.facebook\.watch)/
 
 const LS_SELECTED_ORGANIZATION_KEY = 'mf-id'
 const LS_SELECTED_TOKEN_KEY = 'mf-token'
@@ -21,6 +23,7 @@ export const MAX_PAGINATE_BUTTON_SHOWED = 7
 
 export {
   APP_API_URL,
+  FB_YT_IN_STRING_REGEX,
   LS_SELECTED_ORGANIZATION_KEY,
   LS_SELECTED_TEMPLATE_KEY,
   LS_SELECTED_TOKEN_KEY,
