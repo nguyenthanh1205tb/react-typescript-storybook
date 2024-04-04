351949a78078a6f74aa4b0b171b377149dbcdd8b
aaron nguyen <43309718+nguyenthanh1205tb@users.noreply.github.com>
2024-04-04 10:27:11 +0700

    fix: build, config constants

--- src/views/upload/from-local-files.tsx
+++ src/views/upload/from-local-files.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { ORG_ID, TEMPLATE_ID } from '@/src/configs'
 import UppyDashboard from '@/src/lib/uppy/dashboard'
 import {} from 'react-dropzone'
+import useAppStore from '@/src/stores/useAppStore'
 
 function LocalFilesUpload() {
-  return <UppyDashboard organizationId={ORG_ID} templateId={TEMPLATE_ID} />
+  const { config } = useAppStore()
+
+  return <UppyDashboard organizationId={config?.organizationId as string} templateId={config?.templateId as string} />
 }
 export default LocalFilesUpload
