90dbf307e8c8e154c109f77463d2ad9fe381dcc1
aaron nguyen <43309718+nguyenthanh1205tb@users.noreply.github.com>
2024-04-04 11:51:42 +0700

    init - base

--- src/init.tsx
+++ src/init.tsx
@@ -1,27 +1,8 @@
 import React from 'react'
 import App from '@/src/views/index'
 import ReactDom from 'react-dom/client'
-import { MediaEntity, MediaPackageType } from './types'
 class Init {
-  private usedPackage: MediaPackageType[] = []
-
-  constructor(options?: { type?: MediaPackageType[] }) {
-    if (options) {
-      this.usedPackage = options.type ?? []
-    }
-  }
-
-  render(id: string, options?: { onSubmit: (data: MediaEntity[]) => void }) {
-    if (!id || id === '') return
-    this.appendMediaPackage(id, options?.onSubmit)
-  }
-
-  private appendMediaPackage(ID: string, onSubmit?: (data: MediaEntity[]) => void) {
-    if (!ID) return
-    const place = document.querySelector(ID)
-    if (!place) return
-    const dom = ReactDom.createRoot(place)
-    return dom.render(<App type={this.usedPackage} onExportData={onSubmit} />)
+  constructor() {
   }
 }
 
--- src/views/upload/s3-storage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { PropsWithChildren } from 'react'
-
-interface Props {}
-function S3StorageUpload({}: PropsWithChildren<Props>) {
-  return <div>s3 storage upload</div>
-}
-export default S3StorageUpload
